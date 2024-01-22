489dee92012b853f25944a72e2dfcc01ec7a94e2
Nadiia Yevtushenko <nedilja90@gmail.com>
2024-01-22 21:12:35 +0200

    Second commit

--- Lesson_3.py
+++ Lesson_3.py
@@ -1,3 +1,16 @@
-txt = "Ми є так звані \"Вікінги\" із півночі."
-print(txt)
-# gkhhhhhhhhhhhhhh
+while True:
+    num_1 = float(input("Enter the number_1:"))
+    operation = input("Enter action (+,-, *, /)")
+    num_2 = float(input("Enter the number_2:"))
+    if operation == "+":
+        print(num_1 + num_2)
+    elif operation == "-":
+        print(num_1 - num_2)
+    elif operation == "*":
+        print(num_1 * num_2)
+    elif operation == "/" and num_2 !=0:
+        print(num_1 / num_2)
+    elif operation == "/" and num_2 ==0:
+        print("При діленні дільник не дорівнює 0!")
+    break
+
