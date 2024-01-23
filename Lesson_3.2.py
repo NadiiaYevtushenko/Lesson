4646444337547ac4f1ec25b2ca7142e54b11f469
Nadiia Yevtushenko <nedilja90@gmail.com>
2024-01-23 10:10:29 +0200

    Second commit

---
+++ Lesson_3.2.py
@@ -0,0 +1,36 @@
+#  -------------------------------2
+# list_1 = [12, 3, 4, 10]
+#
+# if len(list_1) > 0:
+#     popped_list_1 = list_1.pop()
+#     last_to_first = popped_list_1
+#     list_1.insert(0, last_to_first)
+#
+# print(list_1)
+#  -------------------------------2
+# list_1 = [1]
+#
+# if len(list_1) > 0:
+#     popped_list_1 = list_1.pop()
+#     last_to_first = popped_list_1
+#     list_1.insert(0, last_to_first)
+#
+# print(list_1)
+#  -------------------------------3
+# list_1 = []
+#
+# if len(list_1) > 0:
+#     popped_list_1 = list_1.pop()
+#     last_to_first = popped_list_1
+#     list_1.insert(0, last_to_first)
+#
+# print(list_1)
+# -------------------------------4
+# list_1 = [12, 3, 4, 10, 8]
+#
+# if len(list_1) > 0:
+#     popped_list_1 = list_1.pop()
+#     last_to_first = popped_list_1
+#     list_1.insert(0, last_to_first)
+#
+# print(list_1)
--- Lesson_3.py
+++ Lesson_3.py
@@ -1,16 +1,21 @@
+# Завдання 1
 while True:
     num_1 = float(input("Enter the number_1:"))
     operation = input("Enter action (+,-, *, /)")
     num_2 = float(input("Enter the number_2:"))
     if operation == "+":
-        print(num_1 + num_2)
+        result = (num_1 + num_2)
     elif operation == "-":
-        print(num_1 - num_2)
+        result =(num_1 - num_2)
     elif operation == "*":
-        print(num_1 * num_2)
+        result =(num_1 * num_2)
     elif operation == "/" and num_2 !=0:
-        print(num_1 / num_2)
+        result =(num_1 / num_2)
     elif operation == "/" and num_2 ==0:
         print("При діленні дільник не дорівнює 0!")
+    print(result)
     break
+
+
+
 
